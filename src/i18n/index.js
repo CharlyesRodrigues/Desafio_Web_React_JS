d8fb5a83c23855a3a02eca905e832fd86c8e384b
CharlyesRodrigues <charlyessouza@gmail.com>
2022-11-13 15:46:51 -0300

    Atualizado

---
+++ src/i18n/index.js
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+
+import {initReactI18next} from 'react-i18next'
+import enJson from './Translations/en.json'
+import espJson from './Translations/esp.json'
+import ptBRJson from './Translations/ptBR.json'
+
+i18n.use(initReactI18next).init({
+
+fallbackLng: "en",
+
+interpolation: {
+
+    escapeValue: false
+ },
+ resources:{
+
+en:enJson,
+esp:espJson,
+ptBR:ptBRJson
+   
+  }
+})
