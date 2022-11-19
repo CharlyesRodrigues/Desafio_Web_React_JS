4c90cc7c3fbe4ed1594352481c008e201e3bd5a3
CharlyesRodrigues <charlyessouza@gmail.com>
2022-11-19 11:58:16 -0300

    Atualizado

--- src/components/Linguagem/index.jsx
+++ src/components/Linguagem/index.jsx
@@ -7,26 +7,33 @@ import { useTranslation } from 'react-i18next'
 import './style.css';
 
 
-const languagesOptions = [
+const languagesPortugues = [
    {
    name: "Português",
    value: "ptBR",
    flag : brasil
    },
-   {
-       name: "Espanhol",
-       value: "esp",
-       flag : spain
-       },
-       {
-           name: "English",
-           value: "en",
-           flag : usa
-       },
+  
    ]
    
+   
+const languagesEspanhol = [
+  {
+  name: "Espanhol",
+  value: "esp",
+  flag : spain
+  },
+ 
+  ]
 
-
+  const languagesEnglish = [
+    {
+    name: "English",
+    value: "en",
+    flag : usa
+    },
+   
+    ]
 
  export function Linguagem() {
   
@@ -37,19 +44,89 @@ const languagesOptions = [
     return (
       <div>
           
+          {languagesPortugues.map((languagesPortugue)=>(
+<button id='background_Portugues'
+key={languagesPortugue.value}
+onClick = {()=>{
+
+i18n.changeLanguage (languagesPortugue.value);
+
+}}
+
+>
+<img className='imagem1' src='src\assets\flags\brasil.png' />
+
+<div >
+<div className='idiomas'>
+<span>{languagesPortugue.name}</span>
+</div>
+</div>
+
+</button>
+
+))}
+
+
+{languagesEspanhol.map((languagesEspanho)=>(
+<button id='background_Espanhol'
+key={languagesEspanho.value}
+onClick = {()=>{
+
+i18n.changeLanguage (languagesEspanho.value);
+
+}}
+
+>
+<img className='imagem1' src='src\assets\flags\spain.png' />
+
+<div >
+<div className='idiomas'>
+<span>{languagesEspanho.name}</span>
+</div>
+</div>
+
+</button>
+
+))}
+
+{languagesEnglish .map((languagesEnglis )=>(
+<button id='background_English'
+key={languagesEnglis.value}
+onClick = {()=>{
+
+i18n.changeLanguage (languagesEnglis.value);
+
+}}
+
+>
+<img className='imagem1' src='src\assets\flags\usa.png' />
+
+<div >
+<div className='idiomas'>
+<span>{languagesEnglis.name}</span>
+</div>
+</div>
+
+</button>
+
+))}
+
+
+
+          
       <div className="background_Portugues"></div>
-       <h1 id='portugues' >Português</h1> 
+
        <button  className="bottomPortugues"> <img src='src\assets\flags\brazil.png'></img></button>
      
       
     
        <div className="background_Ingles"></div>
-       <h1 id="ing" >Inglês</h1> 
+       
        <button type="button" className="bottomIngles" ><img src = 'src/assets/flags/united-states.png'></img></button>
   
    
     <div className="background_Espanhol"> </div>
-       <h1 id='espanhol'  >Espanhol</h1> 
+     
        <button  className="bottomEspanhol"  ><img src='src\assets\flags\spain.png'></img></button>
 
  
